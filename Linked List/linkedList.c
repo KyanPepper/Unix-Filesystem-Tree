2b9d1c1ff13832e50b029bd7d2864bca1616d8b3
KyanPepper <kyankks1@gmail.com>
2024-02-06 23:53:22 -0800

    Test print list

--- Linked List/linkedList.c
+++ Linked List/linkedList.c
@@ -1,100 +1,115 @@
 #include "linkedList.h"
 
-
-
-int initList(List* list){
+int initList(List *list)
+{
     list->size = 0;
     list->head = NULL;
     list->tail = NULL;
 }
-LinkedNode* createNode(void* data){
-   LinkedNode *node = malloc (sizeof(LinkedNode));
-   node->data = data;
-   node->next = NULL;
-   node->prev=NULL;
-   return node;
+LinkedNode *createNode(void *data)
+{
+    LinkedNode *node = malloc(sizeof(LinkedNode));
+    node->data = data;
+    node->next = NULL;
+    node->prev = NULL;
+    return node;
 }
 
-
-int insertTail(List *list, void* data){
+int insertTail(List *list, void *data)
+{
     LinkedNode *node = createNode(data);
-    if(node != NULL){
-        //intialize head
-        if(list->head == NULL || list->size == 0){
+    if (node != NULL)
+    {
+        // intialize head
+        if (list->head == NULL || list->size == 0)
+        {
             list->head = node;
-            list->size+=1;
+            list->size += 1;
             return 1;
         }
-        //intialize tail
-        if(list->tail == NULL){
+        // intialize tail
+        if (list->tail == NULL)
+        {
             list->tail = node;
             list->head->next = list->tail;
             list->tail->prev = list->head;
-            list->size+=1;
+            list->size += 1;
             return 1;
         }
-        //add to end of list
-        LinkedNode* pCur = list->tail; 
+        // add to end of list
+        LinkedNode *pCur = list->tail;
         list->tail = node;
         list->tail->prev = pCur;
         pCur->next = list->tail;
-        list->size+=1;
+        list->size += 1;
         return 1;
     }
     return 0;
 }
 
-void printList(List* list){
+void printList(List *list)
+{
     LinkedNode *pcur = list->head;
-    while(pcur!= NULL){
-        printf(pcur->data);
+    while (pcur != NULL)
+    {
+        printf("%d ", *(int*)pcur->data);
+        pcur = pcur->next;
     }
 }
 
-int removeNode(List* list, LinkedNode* node){
-    if(node != NULL){
-        //remove if head
-        if(node == list->head){
+int removeNode(List *list, LinkedNode *node)
+{
+    if (node != NULL)
+    {
+        // remove if head
+        if (node == list->head)
+        {
             list->head = node->next;
             free(node);
-            list->size-=1;
+            list->size -= 1;
             return 1;
         }
-        //remove if tail
-        if(node == list->tail){
+        // remove if tail
+        if (node == list->tail)
+        {
             list->tail = node->prev;
             free(node);
-            list->size-=1;
+            list->size -= 1;
             return 1;
         }
-        //remove if between head and tail 
+        // remove if between head and tail
         LinkedNode *pcur = node->prev;
         pcur->next = node->next;
         node->prev = pcur;
         free(node);
-        list->size-=1;
+        list->size -= 1;
         return 1;
     }
 
     return 0;
 }
 
-int testInsert(){
+int testInsert()
+{
     List list;
     initList(&list);
     printf("in insert");
-    for(int i = 1; i<=7; i++){
+    for (int i = 1; i <= 7; i++)
+    {
         int *data = malloc(sizeof(int));
         *data = i;
         insertTail(&list, data);
-        }
-        if(list.size == 7){ 
-            LinkedNode* pcur = list.head;
-            for(int i = 1; i<=7; i++){
-                if (pcur != NULL && *(int*)pcur->data != i) {
-                   printf("Value error: %d\n", *(int*)pcur->data);
-                    return 0;
-                }
+    }
+    if (list.size == 7)
+    {
+        LinkedNode *pcur = list.head;
+        for (int i = 1; i <= 7; i++)
+        {
+            if (pcur != NULL && *(int *)pcur->data != i)
+            {
+                printf("Value error: %d\n", *(int *)pcur->data);
+                return 0;
+            }
             pcur = pcur->next;
         }
         return 1;
@@ -103,10 +118,27 @@ int testInsert(){
     return 0;
 }
 
+void testPrintList()
+{
+    List list;
+    initList(&list);
+    printf("in insert");
+    for(int i = 1; i<=7; i++){
+        int *data = malloc(sizeof(int));
+        *data = i;
+        insertTail(&list, data);
+    }
+    printList(&list);
+}
 
-int main(){
-   if(testInsert() == 1){
+int main()
+{
+    if (testInsert() == 1)
+    {
         printf("Insert Passed");
-   }
-   if()
+    }
+
+    printf("Print List Test, Expect 1....7 \n");
+    testPrintList();
+    
 }
