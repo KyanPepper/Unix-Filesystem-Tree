06c920af6e3052adbd061743b255b7b34435ac3d
Kyan <kyankks1@gmail.com>
2024-02-12 21:39:03 -0800

    optimzed and shortend Rmdir

--- src/Commands/Commands.c
+++ src/Commands/Commands.c
@@ -18,34 +18,20 @@ int mkdir(TreeNode *node, char name[64], char *pathname)
 
 int rmdir(TreeNode *node, char *pathname)
 {
-    TreeNode *current = node;
-    TreeNode *parent;
-    LinkedNode* parentOfChild;
-    char *token = strtok(pathname, "/");
-    char *nextToken = NULL;
-    char string[64];
-    TreeNode *child;
-    while (token != NULL)
-    {
-        nextToken = strtok(NULL, "/");
-        strcpy(string, token);
-        child = matchTreeNode(current->children, string);
-        parentOfChild = matchListNode(current->children,string);
-        if ((child == NULL) && (nextToken != NULL))
-        {
-            printf("Error: Directory %s does not exist\n", token);
-            return 0;
-        }
-        parent = current;
-        current = child;
-        token = nextToken;
-    }
-    if((child->children->head != NULL)){
-        printf("Error: Directory %s is not empty\n", token);
+    if(pathname == NULL){
+        printf("No Directory Specified");
         return 0;
     }
-        free(child);
-        removeNode(parent->children,parentOfChild);
+    
+        TreeNode *pCur = cd(node,pathname);
+        if(pCur == NULL){
+            return 0;
+        }
+        TreeNode *parent = pCur->parent;
+        TreeNode *listParentNode = matchListNodeData(parent->children,pCur);
+        free(pCur->children);
+        free(pCur);
+        removeNode(parent->children,listParentNode);
         return 1;
 }
 
