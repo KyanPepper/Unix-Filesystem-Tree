ab5057bdeb3a29e399ecf6a64ac29da36a4755cb
Kyan <kyankks1@gmail.com>
2024-02-13 22:30:43 -0800

    refractored save tree to append parents

--- Serializer/Serializer.c
+++ Serializer/Serializer.c
@@ -1,22 +1,36 @@
 #include "Serializer.h";
 
-void saveTree(TreeNode *node, FILE *file)
+void saveTree(TreeNode *node, FILE *file, char *parents)
 {
     if (node != NULL)
     {
+        char *new_parents;
+        asprintf(&new_parents, "%s/%s", parents, node->name);
         if (node->parent == NULL)
         {
-            fprintf(file, "/\n");
+            fprintf(file, "d /\n");
         }
         else
         {
-            fprintf(file, "/%s\n", node->name);
+            fprintf(file, "%c %s/%s\n", node->type, new_parents, node->name);
         }
-        LinkedNode * child = node->children->head;
+        LinkedNode *child = node->children->head;
         while (child != NULL)
         {
-            saveTree(child,file);
+            saveTree(child, file, parents);
             child = child->next;
         }
+        free(new_parents);
     }
+}
+
+void loadTree(TreeNode *node, FILE *file)
+{
+
+    char line[100];
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+    }
+
+    fclose(file);
 }
--- Serializer/Serializer.h
+++ Serializer/Serializer.h
@@ -1,7 +1,7 @@
 #include "../Tree/Tree.h"
 
 //searializes tree by preorder traversal
-extern void saveTree(TreeNode* node,FILE* file);
+extern void saveTree(TreeNode* node,FILE* file, char *parents);
 
 //desearializes tree
 extern void loadTree(TreeNode* node, FILE file);
