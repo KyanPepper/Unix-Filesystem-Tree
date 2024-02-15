ea2cbe025b9f18a8bb51adf8ba8ab381a103e654
Kyan <kyankks1@gmail.com>
2024-02-15 02:16:48 -0800

    Findcommand

--- src/Console/Console.c
+++ src/Console/Console.c
@@ -1,10 +1,12 @@
 #include "string.h"
+#include "Commands.h"
 #include <stdio.h>
-// returns 1 if input is vaild return 0 if not
+
 int getInput(char *cwd, char *command, char *path, char *name)
 {
     char input[100];
-    printf("%s$ ");
+    pwd(cwd);
+    printf("$");
     fgets(input, sizeof(input), stdin);
     input[strcspn(input, "\n")] = '\0';
     char temp[100];
@@ -40,4 +42,90 @@ int getInput(char *cwd, char *command, char *path, char *name)
         return 0;
     }
     return 1;
+}
+
+#include "Commands.h"
+#include "string.h"
+
+int findCommand(TreeNode *cwd, char *command, char *path, char *name)
+{
+    if (strcmp(command, "mkdir") == 0)
+    {
+        if (name == NULL && path == NULL)
+        {
+            printf("'mkdir' missing operand\n");
+            return 0;
+        }
+        else if (name == NULL)
+        {
+            return mkdir(cwd, path, NULL);
+        }
+        return mkdir(cwd, name, path);
+    }
+    else if (strcmp(command, "rmdir") == 0)
+    {
+        if (name != NULL)
+        {
+            printf("'unknown operand %s'", name);
+            return 0;
+        }
+        return rmdir(cwd, path);
+    }
+    else if (strcmp(command, "creat") == 0)
+    {
+        if (name == NULL && path == NULL)
+        {
+            printf("'creat' missing operand\n");
+            return 0;
+        }
+        else if (name == NULL)
+        {
+            return creat(cwd, path, NULL);
+        }
+        return creat(cwd, name, path);
+    }
+    else if (strcmp(command, "rm") == 0)
+    {
+        if (name != NULL)
+        {
+            printf("'unknown operand %s'", name);
+            return 0;
+        }
+        return rm(cwd, path);
+    }
+    else if (strcmp(command, "cd") == 0)
+    {
+        if (name != NULL)
+        {
+            printf("'unknown operand %s'", name);
+            return 0;
+        }
+        cwd = (cwd, path);
+        return 1;
+    }
+    else if (strcmp(command, "ls") == 0)
+    {
+        if (name != NULL)
+        {
+            printf("'unknown operand %s'", name);
+            return 0;
+        }
+        ls(cwd, path);
+        return 1;
+    }
+    else if (strcmp(command, "pwd") == 0)
+    {
+        if (path != NULL)
+        {
+            printf("'unknown operand %s'", name);
+            return 0;
+        }
+        pwd(cwd);
+        return 1;
+    }
+    else
+    {
+        printf("Unknown command: %s\n", command);
+        return 0;
+    }
 }
--- src/Console/Console.h
+++ src/Console/Console.h
@@ -1,5 +1,9 @@
 #include "../Commands/Commands.h"
-#include "string.h"
+
 //returns 1 if input is vaild return 0 if not
-extern int getInput(char* cwd, char *command, char *path, char *name){
-}
+extern int getInput(char* cwd, char *command, char *path, char *name);
+
+//if else chain for all commands
+extern int findCommand(TreeNode* cwd, char *command, char *path, char *name);
+
+
