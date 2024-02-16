ab238131ff8ba1a5bde6cc54280c55bc1651ec3d
Kyan <kyankks1@gmail.com>
2024-02-15 22:21:56 -0800

    Tree serializer

--- src/Serializer/Serializer.c
+++ src/Serializer/Serializer.c
@@ -5,24 +5,29 @@ void saveTree(TreeNode *node, FILE *file, char *parents)
     if (node != NULL)
     {
         char *new_parents;
-        asprintf(&new_parents, "%s/%s", parents, node->name);
-        if (node->parent == NULL)
+            if(node->parent != NULL && strcmp(node->parent->name,"/") != 0){
+                asprintf(&new_parents, "%s/%s", parents, node->name);
+            }else{
+                asprintf(&new_parents, "%s%s", parents, node->name);
+            }
+        fprintf(file, "%c %s\n", node->type, new_parents); 
+        
+        LinkedNode *child;
+        if (node->children != NULL)
         {
-            fprintf(file, "d /\n");
+            child = node->children->head;
+            while (child != NULL)
+            {
+                TreeNode *newNode = (TreeNode *)child->data;
+                saveTree(newNode, file, new_parents); 
+                child = child->next;
+            }
         }
-        else
-        {
-            fprintf(file, "%c %s/%s\n", node->type, new_parents, node->name);
-        }
-        LinkedNode *child = node->children->head;
-        while (child != NULL)
-        {
-            saveTree(child, file, parents);
-            child = child->next;
-        }
+
         free(new_parents);
     }
 }
+
 
 void loadTree(TreeNode *node, FILE *file)
 {
